faa130029de3ea4daca0ea44a69fe23f50ae2982
wuhaoyong <whywhom@163.com>
2015-01-04 23:34:50 +0800

    why：添加绘图控制，线型，轨道等

--- DMonitor/DMonitor/WorkInfo.h
+++ DMonitor/DMonitor/WorkInfo.h
@@ -11,7 +11,14 @@ public:
 	CString strUnit;//单位
 	int leftLimit;//左边界
 	int rightLimit;//右边界
-	int trackNum;//轨道数
+	int trackNum;//轨道数,0:轨道一；1：轨道二
+	//PS_SOLID /* —*/
+	//PS_DASH  /* -------  */
+	//PS_DOT  /* .......  */
+	//PS_DASHDOT  /* _._._._  */
+	//PS_DASHDOTDOT  /* _.._.._  */
+	int lineType;//线型
+	int lineWidth;//线宽
 	COLORREF curveColor; //颜色
 };
 
